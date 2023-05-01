f3d7220fd86530dab5548130e6856b465122209d
AlexanderVarko <stormss_88@mail.ru>
2023-05-01 16:43:44 +0600

    add photon

---
+++ Assets/Plugins/Scripts/PhotonCameraFix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Cinemachine;
+using Photon.Pun;
+using UnityEngine;
+
+
+namespace Plugins.Scripts
+{
+	public class PhotonCameraFix: MonoBehaviour
+	{
+		[SerializeField]
+		private PhotonView photonView;
+
+
+		[SerializeField]
+		private CinemachineVirtualCamera camera;
+
+
+		private void Awake()
+		{
+			if (!photonView.IsMine)
+			{
+				camera.gameObject.SetActive(false);
+			}
+		}
+	}
+}
