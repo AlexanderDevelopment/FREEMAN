8b5afe2954cea8ae28ae959603f54d58f2a5f711
AlexanderVarko <stormss_88@mail.ru>
2023-05-08 23:24:53 +0600

    add main menu flow

---
+++ Assets/Plugins/Scripts/SoundVolumeProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Doozy.Runtime.UIManager.Components;
+using MoreMountains.Tools;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+namespace Plugins.Scripts
+{
+	public class SoundVolumeProvider : MonoBehaviour
+	{
+		[SerializeField, Required]
+		private MMSoundManager soundManager;
+
+
+		[SerializeField, Required]
+		private UISlider musicVolume;
+
+
+		[SerializeField, Required]
+		private UISlider sfxVolume;
+
+
+		private void Start()
+		{
+			if (!soundManager)
+				soundManager = GetComponent<MMSoundManager>();
+
+			if (musicVolume && sfxVolume)
+			{
+				musicVolume.value = soundManager.GetTrackVolume(MMSoundManager.MMSoundManagerTracks.Music, false);
+				sfxVolume.value = soundManager.GetTrackVolume(MMSoundManager.MMSoundManagerTracks.Sfx, false);
+				musicVolume.OnValueChanged.AddListener(SetMusicVolume);
+				sfxVolume.OnValueChanged.AddListener(SetSfxVolume);
+			}
+			else
+			{
+				Debug.Log($"No have sfx and music sliders in {gameObject.name}");
+			}
+		}
+
+
+		private void SetMusicVolume(float volume)
+			=> soundManager.SetVolumeMusic(volume);
+
+
+		private void SetSfxVolume(float volume)
+			=> soundManager.SetVolumeSfx(volume);
+	}
+}
